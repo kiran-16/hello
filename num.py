4c256e116cd95e48496904d9f6d07dc049a7442f
kiran-16 <68596672+kiran-16@users.noreply.github.com>
2021-09-13 18:16:21 +0530

    Update num.py

--- num.py
+++ num.py
@@ -1,5 +1,6 @@
 import numpy as np
 data=[10,30,45,16,31,50]
-print("mean=",(np.mean))
-print("median=",(np.median))
-print("std deviation=",(np.std))
+print("mean=",(np.mean(data)))
+print("median=",(np.median(data)))
+print("std deviation=",(np.std(data)))
+print("variance=",(np.var(data)))
